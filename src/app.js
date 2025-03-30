955e36ec7da72028be920e498d94621710690c3f
Mohitguleria <mohitguleria888@gmail.com>
2025-03-30 18:27:30 +0530

    nodejs project setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const {v4: uuidv4} = require('uuid')
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Customers App</h1>')
+})
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log("uuid v4", uuidv4())
+    console.log("Server is running on port 3000")
+
+})
